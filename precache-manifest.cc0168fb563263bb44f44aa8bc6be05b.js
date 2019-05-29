aea8e6d5a61d22b95c5710ea77f15b37488f1ffb
Kate <katarinamadness@mail.ru>
2019-05-29 22:43:43 +0300

    Updates

---
+++ precache-manifest.cc0168fb563263bb44f44aa8bc6be05b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  },
+  {
+    "revision": "3fe7f5fb381a1db1f09e",
+    "url": "/goit-react-hw-02/static/js/main.907b89aa.chunk.js"
+  },
+  {
+    "revision": "81fe4c3ba66bcbfe9a9b",
+    "url": "/goit-react-hw-02/static/js/2.269050a8.chunk.js"
+  },
+  {
+    "revision": "3fe7f5fb381a1db1f09e",
+    "url": "/goit-react-hw-02/static/css/main.f52cbe37.chunk.css"
+  },
+  {
+    "revision": "5dcbd4e41cb0fcdfbdcfd74ff7071067",
+    "url": "/goit-react-hw-02/index.html"
+  }
+];
